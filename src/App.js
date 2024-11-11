34a45356129f0c9d43d7aef19f5b64377842a168
Seyoung0914 <seyoung8529@gmail.com>
2024-11-11 15:21:11 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello World!</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          !!Click me!!
+        </a>
+        <p>Thank you~</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
